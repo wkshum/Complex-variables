6648e87739e69a7142d7ddd31f910de9a7afc2c8
Kenneth Shum <158795476+wkshum@users.noreply.github.com>
2024-02-05 14:48:58 +0800

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -1,1 +1,9 @@
-# adf 
+
+
+# Test whethe four points lie on a circle
+def test_concyclic(pt0, pt1, pt2, pt3):
+	z0 = complex(pt0[0],pt0[1])   # convert to complex numbers
+	z1 = complex(pt1[0],pt1[1])
+	z2 = complex(pt2[0],pt2[1])
+	z3 = complex(pt3[0],pt3[1])
+	return abs(((z0-z1)*(z2-z3)/((z0-z3)*(z2-z1))).imag)< 1e-8
