d1bfd2cf3b3a40022cadd24167863868d5b98ae5
Kenneth Shum <kshum2007@gmail.com>
2024-02-06 23:01:54 +0800

    modified:   Section2.lean

--- BrownChurchill/Section2.lean
+++ BrownChurchill/Section2.lean
@@ -61,9 +61,6 @@ theorem Sec2_Ex2b  (z : ℂ) : (I*z).im = z.re := by
 
 example  (z : ℂ) : (I*z).im = z.re := by simp
 
-
-
-
 /-
 Exercise 3. Show that (1+z^2) = 1+2*z+z^2
 -/
@@ -105,8 +102,64 @@ theorem Sec2_Ex6a (a b c : ℂ): (a*b)*c = a*(b*c) := mul_assoc a b c
 theorem Sec2_Ex6b (a b c : ℂ): a*(b+c) = a*b+a*c := mul_add a b c
 
 /-
-Exercrse 7. Use the associative law for addition and the distributive law to show that
-  z(z₁ + z₂ + z₃) = z z₁ + z z₂ + z z₃
+Exercrse 7. Use the associative law for addition and the
+distributive law to show that  z(z₁ + z₂ + z₃) = z z₁ + z z₂ + z z₃
 -/
 
 theorem Sec2_Ex7 (z z₁ z₂ z₃ : ℂ) : z*(z₁ + z₂ + z₃) = z*z₁ + z*z₂ + z*z₃ := by ring
+
+
+/-
+Exercise 8. Prove uniqueness of additive inverse and uniqueness
+        of multiplicative identity
+-/
+
+theorem Sec2_Ex8a (z w₁ w₂ : ℂ) (h₁ : z+w₁ = 0) (h₂ : z+ w₂ = 0) : w₁ = w₂ := by
+  linear_combination h₁ - h₂
+
+theorem Sec2_Ex8b (w : ℂ):  (∀ z, z*w = z) ↔  w = 1 := by
+  constructor
+  · -- forward
+    intro h  -- assume z*w = z for all z
+    have h₁ : 1*w = 1 := by apply h
+    calc
+      w = 1*w  :=  by ring
+      _ = 1 :=  h₁
+  · -- backward
+    intro h  -- assume w=1
+    intro z  -- let z be any complex number
+    calc
+      z*w = z*1 := by rw [h]
+      _ = z := by ring
+
+
+/-
+Exercrse 9. Use −1 = (−1, 0) and z = (x, y) to show that (−1)z = −z
+-/
+
+theorem Sec2_Ex9 (z : ℂ) : (-1)*z = -z := by ring
+
+/-
+Exercise 10. Thus show that the additive inverse of a complex number z = x+iy
+ can be written−z = −x−iy
+-/
+
+theorem Sec2_Ex10 {z : ℂ} {x y : ℝ} (hx : x = z.re) (hy : y = z.im) : -z = -x-I*y := by
+  calc
+    -z = (-1)*z := by ring
+     _ = (-1)*(z.re+(z.im)*I) := by rw [re_add_im z]
+     _ = (-1)*(x+y*I) := by rw [← hx, ← hy]
+     _ = -x -I*y := by ring
+
+/-
+Exercise 11.  Solve the equation z^2 + z + 1 = 0 for z = (x, y) by writing
+(x, y)(x, y) + (x, y) + (1, 0) = (0, 0)
+and then solving a pair of simultaneous equations in x and y.
+-/
+
+theorem Sec2_Ex11 {z: ℂ} {x y : ℝ} (hx : x = z.re) (hy : y = z.im) (hz : z^2+z+1=0) :
+  x=1/2 ∧ (y=(Real.sqrt 3)/2 ∨ y=-(Real.sqrt 3)/2) := by
+  have h₁ : x^2-y^2+x+1 = 0 := by sorry
+  have h₂ : 2*x*y + y =0 := by sorry
+  have h₃ : y ≠ 0 := by sorry
+  have h₄ : y^2 = 3/4 := by sorry
