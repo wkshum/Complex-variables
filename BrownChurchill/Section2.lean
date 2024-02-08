db2f2ab1508bd5cce46adc540db60a45e05d3ee8
Kenneth Shum <kshum2007@gmail.com>
2024-02-08 14:20:41 +0800

    modified:   Section2.lean

--- BrownChurchill/Section2.lean
+++ BrownChurchill/Section2.lean
@@ -155,11 +155,67 @@ theorem Sec2_Ex10 {z : ℂ} {x y : ℝ} (hx : x = z.re) (hy : y = z.im) : -z = -
 Exercise 11.  Solve the equation z^2 + z + 1 = 0 for z = (x, y) by writing
 (x, y)(x, y) + (x, y) + (1, 0) = (0, 0)
 and then solving a pair of simultaneous equations in x and y.
+
+First show that x^2-y^2+x+1=0 and 2*x*y+y = 0
+From the first equation we see that y is not zero.
+Then, the value of x is determined from one of the equations
+After x is known, then y^2 is determined from the other equation.
 -/
 
-theorem Sec2_Ex11 {z: ℂ} {x y : ℝ} (hx : x = z.re) (hy : y = z.im) (hz : z^2+z+1=0) :
-  x=1/2 ∧ (y=(Real.sqrt 3)/2 ∨ y=-(Real.sqrt 3)/2) := by
-  have h₁ : x^2-y^2+x+1 = 0 := by sorry
-  have h₂ : 2*x*y + y =0 := by sorry
-  have h₃ : y ≠ 0 := by sorry
-  have h₄ : y^2 = 3/4 := by sorry
+theorem Sec2_Ex11 {z: ℂ} {x y : ℝ} (hx : x = z.re) (hy : y = z.im)
+ (hz : z^2+z+1=0) :  x=-1/2 ∧ y^2=3/4 := by
+  let w : ℂ := z^2+z+1
+  let u : ℂ := ⟨ x^2-y^2+x+1, 2*x*y+y ⟩
+  -- u is the complex number with real part x^2-y^2+x+1 and imag part 2xy+y                                       -- x^2
+  have h₁ : w = 0 := by   -- obviously we have  w=0
+    calc
+       w = (z^2+z+1) := by dsimp  -- substitute w by definition
+       _ = (0) := by rw [hz]      -- z^2+z+1 = 0 by hypothesis
+  have h₂ : u.re = (x^2-y^2+x+1) := by rfl
+  have h₃ : u.im = (2*x*y+y)     := by rfl
+  have h₄ : w = u := by   -- want to show w is equal to u
+    calc
+      w = z^2+z+1 := by dsimp     -- substitute w by definition
+      _ = (z.re+z.im*I)^2 + (z.re+z.im*I) + 1 := by rw [re_add_im z] -- z = z.re+z.im*I
+      _ = (x+y*I)^2 + (x+y*I) + 1 := by rw [hx , hy]  -- replace z.re = x, z.im = y
+      _ = x^2 + 2*x*y*I + y^2*(I*I) + x+y*I + 1 := by ring  -- expand
+      _ = x^2 + 2*x*y*I + y^2*(-1) + x+y*I + 1 := by rw [I_mul_I]  -- I*I=-1
+      _ = (x^2-y^2+x+1) + (2*x*y+y)*I := by ring  -- simplify
+      _ = ↑((x^2-y^2+x+1):ℝ ) + ↑((2*x*y+y):ℝ )*I := by norm_cast
+      _ = u.re + u.im*I := by rw [h₂ , h₃]
+      _ = u := re_add_im u
+  have h_real : x^2-y^2+x+1 = 0 := by    -- real part of u is 0
+    calc
+    x^2-y^2+x+1 = u.re := by rw [h₂]
+      _ = w.re := (Complex.ext_iff.mp h₄).1.symm
+      _ = (0:ℂ ).re := by rw [← h₁]
+      _ = 0 := rfl
+  have h_imag : (2*x+1)*y = 0 := by         -- imag part of u is 0
+    calc
+      (2*x+1)*y = 2*x*y+y := by ring
+      _ = u.im := by rw [h₃]
+      _ = w.im :=  (Complex.ext_iff.mp h₄).2.symm
+      _ = (0:ℂ).im := by rw [← h₁]
+      _ = 0 := rfl
+  have h₅ : x=-1/2 := by       -- derive x = -1/2 from (2x+1)*y=0 and y≠0
+    have h : y^2 > 0 := by
+      calc
+        y^2 = y^2 + 0 := by ring
+          _ = y^2 + (x^2-y^2+x+1) := by rw [h_real]
+          _ = (x+1/2)^2 + 3/4 := by ring
+          _ > 0 := by positivity
+    have y_ne_zero : y≠ 0 := by apply (sq_pos_iff y).mp h
+    calc
+        x = -1/2 + (1/2)*(2*x+1) := by ring
+        _ = -1/2 + (1/2)*0 := by rw [eq_zero_of_ne_zero_of_mul_right_eq_zero y_ne_zero h_imag]
+        _ = -1/2 := by ring
+  have h₆ : y^2 = 3/4 := by
+    calc
+      y^2 = 3/4 +y^2 - (-1/2)^2 - (- 1/2) - 1 := by ring
+        _ = 3/4 +y^2 - (x)^2 - (x) - 1 := by rw [← h₅]
+        _ = 3/4 - (x^2-y^2+x+1) := by ring
+        _ = 3/4 - 0 := by rw [h_real]
+        _ = 3/4 := by ring
+  constructor
+  · exact h₅
+  · exact h₆
